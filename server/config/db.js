d72b684cd96a48a2540962b75446609b7f7237b2
Sivasudhan <sivasudhan.m.u@gmail.com>
2025-12-05 19:14:51 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+// server/config/db.js  (ESM, named export)
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
